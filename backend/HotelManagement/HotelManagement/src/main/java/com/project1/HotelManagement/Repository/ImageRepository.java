0a117d268c8d80297461749894112a5e34741259
Huỳnh Đức <hiepthaitran2001@gmail.com>
2024-10-15 22:38:53 +0700

    Update repository

--- backend/HotelManagement/HotelManagement/src/main/java/com/project1/HotelManagement/Repository/ImageRepository.java
+++ backend/HotelManagement/HotelManagement/src/main/java/com/project1/HotelManagement/Repository/ImageRepository.java
@@ -1,8 +1,9 @@
 package com.project1.HotelManagement.Repository;
 
+import com.project1.HotelManagement.Entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(path = "image")
-public interface Image extends JpaRepository<Image, Integer> {
+public interface ImageRepository extends JpaRepository<Image, Integer> {
 }
