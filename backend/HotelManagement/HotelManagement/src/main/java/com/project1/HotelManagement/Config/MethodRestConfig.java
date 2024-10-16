fdf12eb37e89ab755e9a856aace8f52c7978964a
Huỳnh Đức <hiepthaitran2001@gmail.com>
2024-10-16 23:05:12 +0700

    BE: Config CORS

---
+++ backend/HotelManagement/HotelManagement/src/main/java/com/project1/HotelManagement/Config/MethodRestConfig.java
@@ -0,0 +1,44 @@
+package com.project1.HotelManagement.Config;
+
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import jakarta.persistence.metamodel.Type;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MethodRestConfig implements RepositoryRestConfigurer {
+
+    private String url = "http://localhost:3000";
+
+    @Autowired
+    private EntityManager entityManager;
+
+
+    // config disabled method
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+
+        config.exposeIdsFor(entityManager.getMetamodel().getEntities().stream().map(Type::getJavaType).toArray(Class[]::new));
+
+        // Cors configuration
+        //cors.addMapping("/**").allowedOrigins(url).allowedMethods("GET","POST","PUT","DELETE");
+//        HttpMethod[] BlocksMethod = {
+//                HttpMethod.POST,
+//                HttpMethod.DELETE,
+//                HttpMethod.PATCH,
+//                HttpMethod.PUT
+//        };
+//        DisableHttpsMethod(Category.class, config, BlocksMethod);
+    }
+
+    private void DisableHttpsMethod(Class c, RepositoryRestConfiguration config, HttpMethod[] methods){
+        config.getExposureConfiguration()
+                .forDomainType(c)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(methods))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(methods));
+    }
+}
