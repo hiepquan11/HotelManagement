0db43f0954fbb1fa87f927f07748580a341b9469
Huỳnh Đức <hiepthaitran2001@gmail.com>
2024-10-31 22:40:16 +0700

    DB: Update database

--- database/hotelmanagement.sql
+++ database/hotelmanagement.sql
@@ -88,3 +88,38 @@ create table image(
     `name` varchar(255),
     `imageUrl` text
 );
+
+use hotelmanagement;
+
+-- Khóa ngoại cho bảng staff
+ALTER TABLE staff
+ADD CONSTRAINT FK_Staff_Role FOREIGN KEY (roleId) REFERENCES role(roleId);
+
+-- Khóa ngoại cho bảng feedback
+ALTER TABLE feedback
+ADD CONSTRAINT FK_Feedback_Customer FOREIGN KEY (customerId) REFERENCES customer(customerId);
+
+-- Khóa ngoại cho bảng room
+ALTER TABLE room
+ADD CONSTRAINT FK_Room_RoomType FOREIGN KEY (roomTypeId) REFERENCES roomtype(roomTypeId);
+
+-- Khóa ngoại cho bảng booking
+ALTER TABLE booking
+ADD CONSTRAINT FK_Booking_Customer FOREIGN KEY (customerId) REFERENCES customer(customerId),
+ADD CONSTRAINT FK_Booking_Room FOREIGN KEY (roomId) REFERENCES room(roomId),
+ADD CONSTRAINT FK_Booking_Staff FOREIGN KEY (staffId) REFERENCES staff(staffId);
+
+-- Khóa ngoại cho bảng payment
+ALTER TABLE payment
+ADD CONSTRAINT FK_Payment_Booking FOREIGN KEY (bookingId) REFERENCES booking(bookingId),
+ADD CONSTRAINT FK_Payment_Customer FOREIGN KEY (customerId) REFERENCES customer(customerId);
+
+-- Khóa ngoại cho bảng useraccount
+ALTER TABLE useraccount
+ADD CONSTRAINT FK_UserAccount_Role FOREIGN KEY (roleId) REFERENCES role(roleId),
+ADD CONSTRAINT FK_UserAccount_Customer FOREIGN KEY (customerId) REFERENCES customer(customerId),
+ADD CONSTRAINT FK_UserAccount_Staff FOREIGN KEY (staffId) REFERENCES staff(staffId);
+
+-- Khóa ngoại cho bảng image
+ALTER TABLE image
+ADD CONSTRAINT FK_Image_RoomType FOREIGN KEY (roomTypeId) REFERENCES roomtype(roomTypeId);
