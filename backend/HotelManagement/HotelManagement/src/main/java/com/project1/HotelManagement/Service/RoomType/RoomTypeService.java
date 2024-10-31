21c5104de1a6031537674ba9e04ef4c56e94dd2a
Huỳnh Đức <hiepthaitran2001@gmail.com>
2024-10-31 15:47:56 +0700

    BE: Function add roomtype with image

--- backend/HotelManagement/HotelManagement/src/main/java/com/project1/HotelManagement/Service/RoomType/RoomTypeService.java
+++ backend/HotelManagement/HotelManagement/src/main/java/com/project1/HotelManagement/Service/RoomType/RoomTypeService.java
@@ -2,9 +2,10 @@ package com.project1.HotelManagement.Service.RoomType;
 
 import com.project1.HotelManagement.Entity.RoomType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface RoomTypeService {
-    public ResponseEntity<?> saveRoomType(RoomType roomType);
+    public ResponseEntity<?> saveRoomType(RoomType roomType, MultipartFile[] files);
     public ResponseEntity<?> updateRoomType(RoomType roomType);
     public ResponseEntity<?> deleteRoomType(RoomType roomType);
 }
