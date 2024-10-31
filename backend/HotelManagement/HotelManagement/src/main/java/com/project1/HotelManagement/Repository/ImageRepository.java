b0567fdebd595294c5b6a5e1233c65861ef57c0e
Huỳnh Đức <hiepthaitran2001@gmail.com>
2024-10-31 22:41:25 +0700

    BE: Function update room type

--- backend/HotelManagement/HotelManagement/src/main/java/com/project1/HotelManagement/Repository/ImageRepository.java
+++ backend/HotelManagement/HotelManagement/src/main/java/com/project1/HotelManagement/Repository/ImageRepository.java
@@ -1,9 +1,15 @@
 package com.project1.HotelManagement.Repository;
 
 import com.project1.HotelManagement.Entity.Image;
+import com.project1.HotelManagement.Entity.RoomType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 @RepositoryRestResource(path = "image")
 public interface ImageRepository extends JpaRepository<Image, Integer> {
+
+    List<Image> findByRoomType(RoomType roomType);
 }
