7c02c5a762606bba8c7b80848b818f2ce6ddd086
Huỳnh Đức <hiepthaitran2001@gmail.com>
2024-11-06 15:51:33 +0700

    BE: decentralization

--- backend/HotelManagement/HotelManagement/src/main/java/com/project1/HotelManagement/Controller/UserAccountController.java
+++ backend/HotelManagement/HotelManagement/src/main/java/com/project1/HotelManagement/Controller/UserAccountController.java
@@ -1,10 +1,13 @@
 package com.project1.HotelManagement.Controller;
 
 import com.project1.HotelManagement.Entity.UserAccount;
+import com.project1.HotelManagement.Security.LoginRequest;
 import com.project1.HotelManagement.Service.UserAccount.UserAccountService;
+import com.project1.HotelManagement.Service.UserAccount.UserAccountServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserAccountController {
 
     @Autowired
-    private UserAccountService userAccountService;
+    private  UserAccountService userAccountService;
+    @Autowired
+    private UserAccountServiceImpl userAccountServiceImpl;
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     @PostMapping("/register")
     private ResponseEntity<?> register(@Validated @RequestBody UserAccount userAccount){
