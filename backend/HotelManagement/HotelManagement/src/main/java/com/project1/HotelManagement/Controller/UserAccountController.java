fabe3b875e1da3d07f2603e619c3782ade0438f5
Huỳnh Đức <hiepthaitran2001@gmail.com>
2024-11-07 14:00:05 +0700

    BE: login with jwt

--- backend/HotelManagement/HotelManagement/src/main/java/com/project1/HotelManagement/Controller/UserAccountController.java
+++ backend/HotelManagement/HotelManagement/src/main/java/com/project1/HotelManagement/Controller/UserAccountController.java
@@ -1,13 +1,19 @@
 package com.project1.HotelManagement.Controller;
 
+import com.project1.HotelManagement.Entity.Response;
 import com.project1.HotelManagement.Entity.UserAccount;
+import com.project1.HotelManagement.Security.JwtResponse;
 import com.project1.HotelManagement.Security.LoginRequest;
+import com.project1.HotelManagement.Service.JWT.JwtService;
 import com.project1.HotelManagement.Service.UserAccount.UserAccountService;
 import com.project1.HotelManagement.Service.UserAccount.UserAccountServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,9 +27,9 @@ public class UserAccountController {
     @Autowired
     private  UserAccountService userAccountService;
     @Autowired
-    private UserAccountServiceImpl userAccountServiceImpl;
-    @Autowired
     private AuthenticationManager authenticationManager;
+    @Autowired
+    private JwtService jwtService;
 
     @PostMapping("/register")
     private ResponseEntity<?> register(@Validated @RequestBody UserAccount userAccount){
@@ -33,5 +39,21 @@ public class UserAccountController {
         } catch (Exception e){
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword())
+            );
+            if(authentication.isAuthenticated()){
+                final String jwt = jwtService.generateToken(loginRequest.getUsername());
+                return ResponseEntity.ok(new JwtResponse(jwt));
+            }
+        } catch (AuthenticationException e){
+            return ResponseEntity.badRequest().body(new Response("Username or password was incorrect", 400));
+        }
+        return ResponseEntity.badRequest().body("Can not authentication");
     }
 }
