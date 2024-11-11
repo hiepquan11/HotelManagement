d0a39c72b29da96f2d6772390480729a14dcd445
Huỳnh Đức <hiepthaitran2001@gmail.com>
2024-11-11 15:37:39 +0700

    BE: function booking

--- backend/HotelManagement/HotelManagement/src/main/java/com/project1/HotelManagement/Repository/BookingRepository.java
+++ backend/HotelManagement/HotelManagement/src/main/java/com/project1/HotelManagement/Repository/BookingRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(path = "booking")
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
+    Booking findByBookingId(int bookingId);
 }
